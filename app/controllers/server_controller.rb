0bb06d4108eddb67fe9711733380600ed36bb1d8
Aurimas Degutis <aurimasdgt@gmail.com>
2015-01-22 00:02:15 +0200

    controllers start ant stop server

--- app/controllers/server_controller.rb
+++ app/controllers/server_controller.rb
@@ -1,7 +1,9 @@
 class ServerController < ApplicationController
   def stop
+    system 'systemctl stop minecraft_blueskies'
   end
 
   def start
+    system 'systemctl start minecraft_blueskies'
   end
 end
