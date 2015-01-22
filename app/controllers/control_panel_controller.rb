ac37c89c9ea23f4b5551d15f0482af6999d86cd4
Aurimas Degutis <aurimasdgt@gmail.com>
2015-01-22 23:08:55 +0200

    primitive experimental file reader

--- app/controllers/control_panel_controller.rb
+++ app/controllers/control_panel_controller.rb
@@ -1,4 +1,24 @@
 class ControlPanelController < ApplicationController
   def show
+    settingsText = String.new
+    file = File.new '/Users/mac/Desktop/Aurimas/minecraft/server.properties', 'r'
+    while line = file.gets
+      settingsText << line
+    end
+    file.close
+  end
+
+  def save
+    settingsText = String.new
+    file = File.new '/Users/mac/Desktop/Aurimas/minecraft/server.properties', 'r'
+    while line = file.gets
+      settingsText << line
+    end
+    file.close
+
+    settingsText << 'lol'
+    file = File.new '/Users/mac/Desktop/Aurimas/minecraft/server.properties', 'w'
+    file.write settingsText
+    file.close
   end
 end
--- app/controllers/server_controller.rb
+++ app/controllers/server_controller.rb
@@ -1,9 +1,13 @@
 class ServerController < ApplicationController
+
   def stop
     system 'systemctl stop minecraft_blueskies'
+    redirectToDefault
   end
 
   def start
     system 'systemctl start minecraft_blueskies'
+    redirectToDefault
   end
+
 end
