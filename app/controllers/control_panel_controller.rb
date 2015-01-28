ce01a772b042897d9800dd0a9998b8bdb556110e
Aurimas Degutis <aurimasdgt@gmail.com>
2015-01-29 00:10:41 +0200

    shows all server properties

--- app/controllers/control_panel_controller.rb
+++ app/controllers/control_panel_controller.rb
@@ -3,13 +3,14 @@ class ControlPanelController < ApplicationController
   def initialize
     @serverPropertiesLocation = '/Users/mac/Desktop/Aurimas/minecraft/server.properties'
   end
+
   def show
-    properties = JavaProperties.load @serverPropertiesLocation
-    properties[:difficulty] = 1
-    JavaProperties.write properties, @serverPropertiesLocation
+    @properties = JavaProperties.load @serverPropertiesLocation
   end
 
   def save
+    properties = JavaProperties.load @serverPropertiesLocation
+    JavaProperties.write properties, @serverPropertiesLocation
     redirectToDefault
   end
 end
