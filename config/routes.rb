5eb6a40465012f5b0d8e99cff2547df4193e62d6
Aurimas Degutis <aurimasdgt@gmail.com>
2015-02-04 00:34:02 +0200

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'control_panel/show'
-  post 'control_panel/save'
-  get 'server/stop'
-  get 'server/start'
+  get '/control_panel/show'
+  post '/control_panel/save'
+  get '/server/stop'
+  get '/server/start'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
